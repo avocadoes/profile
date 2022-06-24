243e9289065e2fff2f528bfb60deacea0d5fa332
avocadoes <mg_zhu@outlook.com>
2022-06-24 13:30:32 -0400

    static export

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,12 @@ import Image from 'next/image'
 import Link from 'next/link'
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos'
 import Header from './components/header'
-import Footer from './components/footer';
+import Footer from './components/footer'
 import pic from '../public/cropped.png'
+
+const customLoader = ({src}) => {
+  return src
+}
 
 export default function Home() {
   return (
@@ -19,8 +23,9 @@ export default function Home() {
         <Header></Header>
         <div className='space'>
         <div className='display center'>
-          <Image src={pic} alt="profile picture" height={450} width={450} />
-          &nbsp;&nbsp;
+          <div className='image'>
+            <Image className='image' src={pic} alt="profile picture" loader={customLoader} />
+          </div>
           <div className='text'>
             <span className='title'>Hello</span>
             <div className='space'>
