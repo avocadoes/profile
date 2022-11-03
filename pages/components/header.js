58f3269cba1d0ddb309b61d66c903e0801918964
avocadoes <mg_zhu@outlook.com>
2022-11-03 19:26:26 -0400

    add menu to header

--- pages/components/header.js
+++ pages/components/header.js
@@ -1,22 +1,49 @@
+import { useState, useEffect } from 'react'
 import Link from 'next/link'
 import DashboardIcon from '@mui/icons-material/Dashboard'
+import MenuIcon from '@mui/icons-material/Menu';
 import { useRouter } from 'next/router'
 import styles from '../../styles/header.module.scss'
 
 export default function Header() {
+    const [show, setShow] = useState(true);
+
+    const handleClick = () => {
+        setShow(!show)
+    }
+
+    useEffect(() => {console.log(show)}, [show])
+
     let about = styles.tab
     let work = styles.tab
     let contact = styles.tab
 
     const route = useRouter().pathname
 
-    if (route == "/about") {
+    if (route === "/about") {
         about += ' ' + styles.current
-    } else if (route == "/work") {
+    } else if (route === "/work") {
         work += ' ' + styles.current
-    } else if (route == "/contact") {
+    } else if (route === "/contact") {
         contact += ' ' + styles.current
     }
+
+    const aboutTab = (
+        <Link href="/about">
+            <a className={about}>ABOUT</a>
+        </Link>
+    )
+    const workTab = (
+        <Link href="/work">
+            <a className={work}>GALLERY</a>
+        </Link>
+    )
+    const contactTab = (
+        <Link href="/contact">
+            <a className={contact}>CONTACT</a>
+        </Link>
+    )
+
     return (
         <div className={styles.container}>
             <Link href="/">
@@ -24,17 +51,22 @@ export default function Header() {
                     <DashboardIcon />&nbsp; MAGGIE ZHU
                 </a>
             </Link>
+            <button className={styles.menu} onClick={handleClick}>
+                <MenuIcon />
+            </button>
             <span className={styles.tabs}>
-                <Link href="/about">
-                    <a className={about}>ABOUT</a>
-                </Link>
-                <Link href="/work">
-                    <a className={work}>GALLERY</a>
-                </Link>
-                <Link href="/contact">
-                    <a className={contact}>CONTACT</a>
-                </Link>
+                {aboutTab}
+                {workTab}
+                {contactTab}
             </span>
+            {show && ( 
+            <span className={styles.mobile}>
+                {aboutTab}
+                {workTab}
+                {contactTab}
+            </span>
+            )}
+            
         </div>
     )
 }
